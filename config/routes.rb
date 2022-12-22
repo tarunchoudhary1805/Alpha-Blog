91091fe1526f3a2f8fdda8a570af5d12e174ad48
Tarun Choudhary <tarunchoudhary1805@gmail.com>
2022-12-22 16:04:50 +0530

    scaffold generated

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :articles, only: [:show]
   root "pages#home"
   get "about",to:"pages#about"
+  # resources :Articles
 end
--- db/migrate/20221222101402_create_articles.rb
+++ db/migrate/20221222101402_create_articles.rb
@@ -3,8 +3,8 @@ class CreateArticles < ActiveRecord::Migration[7.0]
     create_table :articles do |t|
       t.string :title
       t.text :description
+
       t.timestamps
     end
   end
 end
-  
