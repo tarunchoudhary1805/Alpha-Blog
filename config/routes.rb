723886171e40db63d21fc804842a571e4ed2f184
Tarun Choudhary <tarunchoudhary1805@gmail.com>
2022-12-23 19:59:08 +0530

    added article listing page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :articles,only: [:show]
+  resources :articles,only: [:show, :index]
   root 'pages#home'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
