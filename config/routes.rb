7b76d0fb54c90407173932b67648f8b73221cd02
Tarun Choudhary <tarunchoudhary1805@gmail.com>
2022-12-23 19:47:49 +0530

    show controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  resources :articles
+  resources :articles,only: [:show]
+  root 'pages#home'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
