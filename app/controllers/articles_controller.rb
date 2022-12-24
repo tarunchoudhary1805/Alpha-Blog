0acb548d44e0c705887c66da06bb778710a5db63
Tarun Choudhary <tarunchoudhary1805@gmail.com>
2022-12-24 21:10:03 +0530

    new article form

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -8,4 +8,10 @@ class ArticlesController < ApplicationController
     @articles = Article.all
  end
 
+ def create
+    @article = Article.new(params.require(:article).permit(:title, :description))
+    @article.save
+    redirect_to (@article)
+ end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :articles,only: [:show, :index]
+  resources :articles,only: [:show, :index, :new, :create]
   root 'pages#home'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
