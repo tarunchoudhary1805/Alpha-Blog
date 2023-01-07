1789e7e585b746878663a6c54e772a9fd086e680
Tarun Choudhary <tarunchoudhary1805@gmail.com>
2023-01-07 18:39:27 +0530

    Clean

--- config/routes.rb
+++
@@ -1,8 +0,0 @@
-Rails.application.routes.draw do
-  resources :articles,only: [:show, :index, :new, :create]
-  root 'pages#home'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
-end
